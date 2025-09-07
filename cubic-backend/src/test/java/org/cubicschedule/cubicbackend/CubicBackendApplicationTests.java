4adbd44ae549a13f6e9d5d92f8631347d0267f56
Yaroslav <136057427+Jabka00@users.noreply.github.com>
2025-09-07 14:57:18 +0300

    minor changes

---
+++ cubic-backend/src/test/java/org/cubicschedule/cubicbackend/CubicBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package org.cubicschedule.cubicbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CubicBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
