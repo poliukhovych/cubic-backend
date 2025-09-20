9aa637ff8ed87f33e1e98de0472dca93be141c3f
Yaroslav <136057427+Jabka00@users.noreply.github.com>
2025-09-20 17:07:04 +0300

    hotfix local DB config for Yaroslav setup :)

--- run.py
+++ run.py
@@ -1,4 +1,18 @@
 import uvicorn
+from dotenv import load_dotenv
+import os
+
+load_dotenv('.env.development')
+load_dotenv('.env')
+
+if not os.getenv('DATABASE_URL'):
+    os.environ['DATABASE_URL'] = 'postgresql+asyncpg://postgres:1234@localhost:5432/schedule_db'
+if not os.getenv('POSTGRES_USER'):
+    os.environ['POSTGRES_USER'] = 'postgres'
+if not os.getenv('POSTGRES_PASSWORD'):
+    os.environ['POSTGRES_PASSWORD'] = '1234'
+if not os.getenv('POSTGRES_DB'):
+    os.environ['POSTGRES_DB'] = 'schedule_db'
 
 if __name__ == "__main__":
     uvicorn.run(
