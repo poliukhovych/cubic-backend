68e64b96e0a8344070a884b56fecb5450fd597e5
Yaroslav <136057427+Jabka00@users.noreply.github.com>
2025-09-21 00:03:47 +0300

    add teachers tests + minor changes:)

--- app/core/deps.py
+++ app/core/deps.py
@@ -1,10 +1,9 @@
-from fastapi import Request, Depends
+from fastapi import Request
 
 from app.db.session import async_session_maker
 from app.services.course_service import CourseService
 from app.services.teacher_service import TeacherService
 from app.services.group_service import GroupService
-from sqlalchemy.ext.asyncio import AsyncSession
 
 
 async def get_session():
@@ -16,8 +15,8 @@ def get_course_service(request: Request) -> CourseService:
     return request.app.state.course_service
 
 
-async def get_teacher_service(session: AsyncSession = Depends(get_session)) -> TeacherService:
-    return TeacherService(session)
+def get_teacher_service(request: Request) -> TeacherService:
+    return request.app.state.teacher_service
 
 
 def get_group_service(request: Request) -> GroupService:
