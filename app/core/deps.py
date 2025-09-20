58b634b35cb7df7eb1a2b0e03f693b9591025c96
Yaroslav <136057427+Jabka00@users.noreply.github.com>
2025-09-20 18:00:52 +0300

    done

--- app/core/deps.py
+++ app/core/deps.py
@@ -1,13 +1,19 @@
-from fastapi import Request
+from fastapi import Request, Depends
 
 from app.db.session import async_session_maker
 from app.services.course_service import CourseService
 from app.services.teacher_service import TeacherService
 from app.services.group_service import GroupService
+from sqlalchemy.ext.asyncio import AsyncSession
 
 
-def get_course_service(request: Request) -> CourseService:
-    return request.app.state.course_service
+async def get_session():
+    async with async_session_maker() as session:
+        yield session
+
+
+async def get_course_service(session: AsyncSession = Depends(get_session)) -> CourseService:
+    return CourseService(session)
 
 
 def get_teacher_service(request: Request) -> TeacherService:
@@ -16,8 +22,3 @@ def get_teacher_service(request: Request) -> TeacherService:
 
 def get_group_service(request: Request) -> GroupService:
     return request.app.state.group_service
-
-
-async def get_session():
-    async with async_session_maker() as session:
-        yield session
